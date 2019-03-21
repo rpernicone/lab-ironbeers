6dfedc22b674eb17a2c6c4d1c4f9e35b69bbce34
rpernicone <roberto.pernicone@gmail.com>
2019-03-20 22:31:30 -0600

    everything there but no rendering

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,6 +8,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -15,7 +16,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res, next) => {
   res.render('index');
 });
-
+app.get('/beers', (req, res, next) =>{
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', beers)
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+app.get('/random-beers', (req, res, next) =>{
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('beers')
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
 
 
 app.listen(3000);
